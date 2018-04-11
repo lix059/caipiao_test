c9138916d3727b169c596b72793035a531ed028d
lixiong <lixiong@turingcat.com>
2018-04-11 10:01:33 +0800

    修改为递归方法

--- caipiao.js
+++ caipiao.js
@@ -1,45 +1,26 @@
-function gen(arr, n) {
-  let start_one = 0;
-  let start_two = start_one + 1;
-  let start_end = start_two + n - 2;
-  let tmp = [], tmp_line=[];
-  while(start_one < arr.length-n+1) {
-    tmp[0] = arr[start_one];
-    start_two = start_one + 1;
-    
-    while(start_two < arr.length-n+2) {
-      for(let i=start_two, j=1; i<=start_two+n-2; i++,j++) {
-        tmp[j] = arr[i];
-      }
-      start_end = start_two + n - 2;
-      while(start_end < arr.length) {
-        tmp[n-1] = arr[start_end++];
-        tmp_line.push(tmp.join(','));
-      }
-      console.log(tmp_line);
-      tmp_line=[];
-      start_two++;
-    }
-    start_one++;
-  }
 
+function gen(arr, n) {
+  if(n === 1) {
+    let result = [];
+    arr.forEach(item => {
+      result.push([item]);
+    });
+    return result;
+  }
+  if(arr.length === n) {
+    return [[...arr]];
+  }
+  return comb(arr[0], gen(arr.slice(1), n-1)).concat(gen(arr.slice(1), n))
 }
 
-gen([1,2,3,4,5,6,7], 3);
+function comb(e, arr) {
+  let i = 0;
+  while(i < arr.length) {
+    arr[i].push(e);
+    i++;
+  }
+  return arr;
+}
 
-// output
-// [ '1,2,3', '1,2,4', '1,2,5', '1,2,6', '1,2,7' ]
-// [ '1,3,4', '1,3,5', '1,3,6', '1,3,7' ]
-// [ '1,4,5', '1,4,6', '1,4,7' ]
-// [ '1,5,6', '1,5,7' ]
-// [ '1,6,7' ]
-// [ '2,3,4', '2,3,5', '2,3,6', '2,3,7' ]
-// [ '2,4,5', '2,4,6', '2,4,7' ]
-// [ '2,5,6', '2,5,7' ]
-// [ '2,6,7' ]
-// [ '3,4,5', '3,4,6', '3,4,7' ]
-// [ '3,5,6', '3,5,7' ]
-// [ '3,6,7' ]
-// [ '4,5,6', '4,5,7' ]
-// [ '4,6,7' ]
-// [ '5,6,7' ]
+
+console.log(gen([1,2,3,4,5,6,7], 3));
